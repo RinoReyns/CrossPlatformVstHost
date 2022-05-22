c1fb4b5a5ae6285226cd7650590a2eaa796736fb
RinoReyns <lukaszpindor1@gmail.com>
2022-05-22 18:06:13 +0200

    try to run ut on anroid emulator

--- VstHost_Android/app/src/androidTest/java/com/rinoreyns/vsthost_android/ExampleInstrumentedTest.java
+++ VstHost_Android/app/src/androidTest/java/com/rinoreyns/vsthost_android/ExampleInstrumentedTest.java
@@ -1,7 +1,10 @@
 package com.rinoreyns.vsthost_android;
 
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
 
+import androidx.lifecycle.Lifecycle;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -10,6 +13,13 @@ import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -17,10 +27,101 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.rinoreyns.vsthost_android", appContext.getPackageName());
+    static {
+        try {
+            System.loadLibrary("VstHostLibWrapper");
+        }
+        catch (Exception e) {
+            System.out.println("Oops!");
+        }
     }
+    public static String [] platforms_prefixes  =
+            {
+                    "armeabi-v7a-android",
+                    "arm64-v8a-android",
+                    "x86-android",
+                    "x86_64-android"
+            };
+    public native int intFromJNI(String plugin_path);
+    public native String getPlatformId();
+
+    private void getAssetAppFolder(String dir, String suffix) throws Exception{
+        File f = new File(dir);
+        if (!f.exists() || !f.isDirectory())
+            f.mkdirs();
+
+        AssetManager am = InstrumentationRegistry.getInstrumentation().getTargetContext().getAssets();
+        String [] aplist = am.list(suffix);
+
+        for(String strf:aplist){
+            try
+            {
+                InputStream is = am.open(strf);
+                copyToDisk(dir, strf, is);
+            }
+            catch(Exception ex)
+            {
+                if (suffix.equals(""))
+                {
+                    getAssetAppFolder(dir+"/"+strf, strf);
+                }
+                else
+                {
+                    if (Arrays.asList(platforms_prefixes).contains(strf))
+                    {
+                        String platform_id = getPlatformId();
+                        getAssetAppFolder(dir+"/" + platform_id + "-linux", suffix + "/" + strf);
+                    }
+                    else
+                    {
+                        getAssetAppFolder(dir+"/"+strf, suffix + "/" + strf);
+                    }
+                }
+            }
+        }
+    }
+
+    public void copyToDisk(String dir,String name,InputStream is) throws IOException{
+        int size;
+        byte[] buffer = new byte[2048];
+
+        FileOutputStream fout = new FileOutputStream(dir+"/" +name);
+        BufferedOutputStream bufferOut = new BufferedOutputStream(fout, buffer.length);
+
+        while ((size = is.read(buffer, 0, buffer.length)) != -1) {
+            bufferOut.write(buffer, 0, size);
+        }
+        bufferOut.flush();
+        bufferOut.close();
+        is.close();
+        fout.close();
+    }
+
+    @Test
+    public void SetSimpleFlowForLib()
+    {
+        String copy_dest_folder = InstrumentationRegistry.getInstrumentation().getTargetContext().getFilesDir().toString() + "/Devices";
+        File file = new File(copy_dest_folder);
+
+        if (file.exists()) {
+            String deleteCmd = "rm -r " + copy_dest_folder;
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec(deleteCmd);
+            } catch (IOException e) { }
+        }
+
+        try
+        {
+            getAssetAppFolder(copy_dest_folder, "");
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        int test = intFromJNI( copy_dest_folder );
+        assertEquals(0, test);
+        android.util.Log.d("tag" , "Test");
+    }
+
 }
--- VstHost_Android/app/src/main/cpp/VstHostLibWrapper.cpp
+++ VstHost_Android/app/src/main/cpp/VstHostLibWrapper.cpp
@@ -48,4 +48,47 @@ extern "C"
         return static_cast<jint>(status);
     }
 
+    JNIEXPORT jstring JNICALL
+    Java_com_rinoreyns_vsthost_1android_ExampleInstrumentedTest_getPlatformId(JNIEnv *env, jobject thiz) {
+        struct utsname unameData;
+        int res = uname (&unameData);
+        if (res != 0)
+            return {};
+        return (env)->NewStringUTF(unameData.machine);
+    }
+
+    JNIEXPORT jint JNICALL
+    Java_com_rinoreyns_vsthost_1android_ExampleInstrumentedTest_intFromJNI(JNIEnv *env, jobject thiz, jstring path) {
+        std::unique_ptr<AudioProcessingVstHost> test(new AudioProcessingVstHost());
+        jboolean isCopy;
+
+        const char *convertedValue = (env)->GetStringUTFChars(path, &isCopy);
+        std::string str = convertedValue;
+        int status = 0;
+        try {
+            status = test->CreatePluginInstance(str + "/adelay.vst3", "plugin_1");
+            status = test->ProcessWaveFile(str + "/sine_440.wav", str + "out.wav");
+            test->Terminate();
+            // TODO:
+            // add wave_io lib
+    //            if (status == 0)
+    //            {
+    //                wave::File input_wave_file;
+    //                if (input_wave_file.Open(wave_path, wave::kIn))
+    //                {
+    //                    return VST_ERROR_STATUS::OPEN_FILE_ERROR;
+    //                }
+    //
+    //            }
+        }
+        catch(...)
+        {
+            std::exception_ptr p = std::current_exception();
+            std::cout << "" << std::endl;
+        }
+        return static_cast<jint>(status);
+    }
+
+
 }
+
