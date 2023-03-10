18aaf3fd97a36992473607b31347c673eb78f6a9
Rino Reyns <lukaszpindor1@gmail.com>
2023-03-10 17:29:31 +0100

    clean up code

--- VstHost_VisualC++/modules/AudioProcessing/header/WaveProcessingPipeline.h
+++ VstHost_VisualC++/modules/AudioProcessing/header/WaveProcessingPipeline.h
@@ -15,12 +15,11 @@ public:
     int Init(config_type vst_host_config);
     int GetConfig();
     int Run(std::string input_path, std::string output_path);
-    
 
 private:
     BWLowPass* bw_low_pass_filter_ = nullptr;
     uint8_t verbosity_ = 0;
-    config_type vst_host_config_ {};
+    config_type vst_host_config_{};
     std::unique_ptr<AudioProcessingVstHost> vst_host_;
 };
 
