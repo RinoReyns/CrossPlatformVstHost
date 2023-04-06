414c5baf6d498d7b649eeff3e86739050653d1c8
Rino Reyns <lukaszpindor1@gmail.com>
2023-04-06 22:35:23 +0200

    check if exclusion in code works

--- VstHost_VisualC++/modules/VstHostTool/src/main.cpp
+++ VstHost_VisualC++/modules/VstHostTool/src/main.cpp
@@ -1,7 +1,7 @@
 #include "VstHostTool.h"
 #include <memory>
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[])  // GCOVR_EXCL_START
 {   
     std::unique_ptr<VstHostTool> vst_host_tool(new VstHostTool());
     int status = vst_host_tool->PrepareArgs(argc, argv);
@@ -10,4 +10,5 @@ int main(int argc, char* argv[])
         return status;
     }
     return vst_host_tool->Run();   
-}
+}  // GCOVR_EXCL_STOP
+
